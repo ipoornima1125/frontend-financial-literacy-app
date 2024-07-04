421efe0e94aa1636800d1d3d0cb99c1ab3c6c8f7
ipoornima1125 <Poornima2004.11@rediffmail.com>
2024-07-04 12:41:06 +0530

    added BudgetQuiz

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import BudgetQuiz from "./BudgetQuiz";
+import {useState} from "react";
 function Cards({item}) {
+  const [showQuiz, setShowQuiz] = useState(false);
+
+  const handleTakeQuizClick = () => {
+    setShowQuiz(true);
+  };
   return (
    <>
      <div className="flex flex-col md:flex-row p-3 hover:scale-105 duration-200 ">
@@ -13,9 +20,17 @@ function Cards({item}) {
     </h2>
     <p>{item.title}</p>
     <div className="card-actions justify-between">
-    <Link to={item.quiz}>
-  <button className="bg-orange-300 font-playwriteNGModern text-black px-4 py-2 mt-12 rounded-md hover:bg-orange-400 duration-300">Take a Quiz</button>
-</Link>
+    
+  
+      <button
+        className="bg-orange-300 font-playwriteNGModern text-black px-4 py-2 mt-12 rounded-md hover:bg-orange-400 duration-300"
+        onClick={handleTakeQuizClick}
+      >
+        Take a Quiz
+      </button>
+      {showQuiz && <BudgetQuiz />}
+    
+
 
 
       <Link to={item.link}>
